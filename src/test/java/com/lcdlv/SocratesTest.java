8a8d8a524461e0924558faeac36a0a13147af3fb
Hacene <hacene.upmc@gmail.com>
2020-03-11 10:41:17 +0100

    initialize kata

---
+++ src/test/java/com/lcdlv/SocratesTest.java
@@ -0,0 +1,14 @@
+package com.lcdlv;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SocratesTest {
+
+    @Test
+    public void settingTest() {
+        assertThat(true).isTrue();
+    }
+}
