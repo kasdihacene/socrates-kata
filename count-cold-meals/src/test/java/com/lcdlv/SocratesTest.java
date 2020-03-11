67c1e62b52d9dff71ce549f5552b268805ffc5a6
Hacene <hacene.upmc@gmail.com>
2020-03-11 12:30:15 +0100

    remove old tests

--- count-cold-meals/src/test/java/com/lcdlv/SocratesTest.java
+++ count-cold-meals/src/test/java/com/lcdlv/SocratesTest.java
@@ -7,19 +7,7 @@ import static org.assertj.core.api.Assertions.*;
 public class SocratesTest {
 
     @Test
-    public void settingTest() {
-        assertThat(true).isTrue();
-    }
-
-    @Test
-    public void countColdMeals() {
-        String records = "toto, jean, repas froid";
-        boolean isRepasFroid = records.contains("repas froid");
-        assertThat(isRepasFroid).isTrue();
-    }
-
-    @Test
-    public void checkIn_whenColdMeal(){
+    public void checkIn_whenColdMeal() {
         // Arrange
         CheckIn checkIn1 = new CheckIn("J", 21);
         //Act
@@ -36,7 +24,7 @@ public class SocratesTest {
     }
 
     @Test
-    public void checkIn_WhenColdMealAt2(){
+    public void checkIn_WhenColdMealAt2() {
         CheckIn checkIn = new CheckIn("J", 00);
         boolean coldMeal = checkIn.isColdMeal();
         assertThat(coldMeal).isTrue();
