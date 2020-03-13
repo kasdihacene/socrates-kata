d12137847dc82d15e50b9d6597901d5f7bbd23dd
Hacene <hacene.upmc@gmail.com>
2020-03-13 09:38:41 +0100

    add two case : twin and triple room

--- price-calculator/src/main/java/com/lcdlv/PriceCalculator.java
+++ price-calculator/src/main/java/com/lcdlv/PriceCalculator.java
@@ -1,7 +1,13 @@
 package com.lcdlv;
 
 public class PriceCalculator {
+
     public static int calculateParticipationPrice(String participant) {
+        if (participant.contains("Twin")) {
+            return 510;
+        }else if (participant.contains("Triple")){
+            return 410;
+        }
         return 610;
     }
 }
--- price-calculator/src/test/java/com/lcdlv/PriceCalculatorTest.java
+++ price-calculator/src/test/java/com/lcdlv/PriceCalculatorTest.java
@@ -7,6 +7,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PriceCalculatorTest {
 
     public static final int SINGLE_ROOM_PRICE = 610;
+    public static final int TWIN_ROOM_PRICE = 510;
+    public static final int TRIPLE_ROOM_PRICE = 410;
 
     @Test
     public void payCompletePriceWithSingleChoice() {
@@ -14,5 +16,19 @@ public class PriceCalculatorTest {
         String pierre = "Pierre;Thursday 20;Sunday 14;Single";
         int participationPrice = PriceCalculator.calculateParticipationPrice(pierre);
         assertThat(participationPrice).isEqualTo(SINGLE_ROOM_PRICE);
+    }
+
+    @Test
+    public void payCompletePriceWithTwinChoice(){
+        String mila = "Mila;Thursday 18;Sunday 15;Twin";
+        int participationPrice = PriceCalculator.calculateParticipationPrice(mila);
+        assertThat(participationPrice).isEqualTo(TWIN_ROOM_PRICE);
+    }
+
+    @Test
+    public void payCompletePriceWithTripleChoice(){
+        String john = "John;Thursday 18;Sunday 15;Triple";
+        int participationPrice = PriceCalculator.calculateParticipationPrice(john);
+        assertThat(participationPrice).isEqualTo(TRIPLE_ROOM_PRICE);
     }
 }
